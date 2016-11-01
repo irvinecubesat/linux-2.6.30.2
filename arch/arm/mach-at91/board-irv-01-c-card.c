652c0e7289e17ea39f67fea24dba1aa11842ed4c
Maurice Ling <maurice@linglink.com>
2016-10-31 23:36:38 -0700

    add i2c expander initialization

--- arch/arm/mach-at91/board-irv-01-c-card.c
+++ arch/arm/mach-at91/board-irv-01-c-card.c
@@ -33,18 +33,52 @@
 
 #define DBB_EXPANDER_BASE 296
 
+
 /**
- * Setup the expander control register
+ * Setup the gpio pins to proper initialize states.
+ * 
+ * Ports are set up to be output pins
+ *
+ * Function    Port  gpio
+ * ----------------------
+ * DSA1_RLS_B  P0    296
+ * DSA1_DPLY_B P1    297
+ * DSA2_RLS_B  P2    298
+ * DSA2_DPLY_B P3    299
+ * MT01_EN     P4    300 <--- set low at initialization
+ * MT02_EN     P5    301 <--- set low at initialization
+ * MT03_EN     P6    302 <--- set low at initialization
+ * DSA_EN_BAR  P7    303
  **/
 int pcf857x_setup(struct i2c_client *client,
-		  int gpio, unsigned ngpio,
-		  void *context)
+		    int gpio, unsigned ngpio,
+		    void *context)
 {
+  int result=0;
+  int i = gpio;
+  for (i = gpio; i < ngpio; i++)
+    {
+      int setValue=1;
+      int status=0;
+      if (i >=300 && i <= 302)	/* MT01-03 should be turned off to start*/
+	{
+	  setValue=0;
+	}
+      status=gpio_direction_output(gpio, setValue);
+      if (status != 0)
+	{
+	  dev_err(&client->dev, "Error %d setting gpio %d to output\n",
+		  status, i);
+	  result=-1;
+	}
+    }
+  return result;
 }
 
 static struct pcf857x_platform_data dbb_expander_plat_data = {
    .gpio_base = DBB_EXPANDER_BASE,
-   .invert = 0,
+   .setup = pcf857x_setup,
+   .n_latch=0
 };
 
 struct i2c_info {
@@ -56,9 +90,9 @@ static struct i2c_info i2c_dbb_devices[] = {
    {
       .client = NULL,
       .board_info = {
-         I2C_BOARD_INFO("tca9554a", 0x70),
+         I2C_BOARD_INFO("tca9554a", 0x38),
          .platform_data = &dbb_expander_plat_data,
-      },
+      }
    },
 };
 #define NUM_DBB_I2C_DEVICES (sizeof(i2c_dbb_devices) / sizeof(i2c_dbb_devices[0]))
@@ -66,20 +100,19 @@ static struct i2c_info i2c_dbb_devices[] = {
 static void __exit cleanup_board(void)
 {
    int i;
-
-   for (i = 0; i < NUM_DBB_I2C_DEVICES; i++) {
-      if (i2c_dbb_devices[i].client) {
+   for (i=0; i <NUM_DBB_I2C_DEVICES; i++)
+     {
+       if (i2c_dbb_devices[i].client) {
          i2c_unregister_device(i2c_dbb_devices[i].client);
          i2c_dbb_devices[i].client = NULL;
-      }
-   }
+       }
+     }
 }
 
 static int __init setup_board(void)
 {
-   int i;
+   int i=0;
    struct i2c_adapter *adap = NULL;
-   int res;
 
    adap = i2c_get_adapter(1);
    if (!adap) {
@@ -100,7 +133,6 @@ static int __init setup_board(void)
    return 0;
 err:
 
-   cleanup_board();
    return -1;
 }
 
